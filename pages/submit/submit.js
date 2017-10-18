76c07c7640dc68077d34d45ab5e7d34cf4a6491a
caoxiaosheng <836900367@qq.com>
2017-10-19 01:10:57 +0800

    实现部分问题模板

--- pages/submit/submit.js
+++ pages/submit/submit.js
@@ -1,74 +1,88 @@
 // pages/submit/submit.js
 Page({
 
+  /**
+     * 页面的初始数据
+     */
+  data: {
+    clearValue:""
+  },
+
   formSubmit: function (e) {
     console.log('form发生了submit事件，携带数据为：', e.detail.value)
+    this.setData({
+      clearValue:""
+    })
   },
 
-  formReset: function () {
-    console.log('form发生了reset事件')
+  onReset: function () {
+    var that=this
+    wx.showModal({
+      title: '提示',
+      content: '是否清除所有信息？',
+      confirmColor:'#e51c23',
+      success: function (res) {
+        if (res.confirm) {
+          that.setData({
+            clearValue: ""
+          })
+        }
+      }
+    })
   },
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-  
-  },
-
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   }
 })
